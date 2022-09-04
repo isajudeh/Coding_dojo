0684fdb878ff018e12e16e46ab5a1a105aa260c7
isajudeh <isajudeh@yahoo.com>
2022-09-04 23:39:09 +0300

    new

---
+++ CodingDojo/WebFundamentals/JS/Liking Likes/likes2.js
@@ -0,0 +1,20 @@
+var count1 =9;
+
+function add1(){
+    count1++;
+    document.querySelector(".c12").innerText = count1 + " likes(s)";
+}
+
+var count2 =12;
+
+function add2(){
+    count2++;
+    document.querySelector(".c22").innerText = count2 + " likes(s)";
+}
+
+var count3 =9;
+
+function add3(){
+    count3++;
+    document.querySelector(".c32").innerText = count3 + " likes(s)";
+}
