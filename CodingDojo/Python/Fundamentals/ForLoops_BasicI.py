5e19dc96432e54a7ad5b449ac751bcceeef5b64b
isajudeh <isajudeh@yahoo.com>
2022-09-12 15:18:15 +0300

    loop

---
+++ CodingDojo/Python/Fundamentals/ForLoops_BasicI.py
@@ -0,0 +1,33 @@
+# for x in range(0,150):
+#     print(x)
+# # ######################## #
+# for x in range(5,1000):
+#     if x%5==0:
+#         print(x)
+# # ######################## #
+# for x in range(1,100):
+#     if x%10==0:
+#         print("Coding Dojo")
+#     elif x%5==0:
+#         print("Coding")
+#     else:
+#         print(x)
+# # ######################## #
+# sum=0
+# for x in range(0,500000):
+#     if x%2==1:
+#         sum=sum+x
+# print(sum)
+# # ######################## #
+# x=2018
+# while x>0:
+#     print(x)
+#     x=x-4
+# # ######################## #
+int lowNum
+int highNum
+int mult
+for x in range(lowNum,highNum):
+    if x%mult==0:
+        print(x)
+# # ######################## #
