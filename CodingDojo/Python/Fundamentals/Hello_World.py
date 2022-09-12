60c6420d4c0995174243b4b8adae8d4def06cb3d
isajudeh <isajudeh@yahoo.com>
2022-09-12 11:36:26 +0300

    first

---
+++ CodingDojo/Python/Fundamentals/Hello_World.py
@@ -0,0 +1,11 @@
+print("Hello World")
+a="ISA"
+print("hello",a)
+print("hello"+a)
+b="1"
+print("hello",b)
+print("hello"+ b)
+food_one="kabab"
+food_two="banana"
+print("I love eat",food_one,"and",food_two)
+print("I love eat %s and %s" %(food_one,food_two))
