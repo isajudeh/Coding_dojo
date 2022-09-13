e4b4bfb09f86b222305899e701e0135d43d7c4fb
isajudeh <isajudeh@yahoo.com>
2022-09-13 13:38:18 +0300

    calculator

--- CodingDojo/WebFundamentals/JS/Calculator/Calculator.js
+++ CodingDojo/WebFundamentals/JS/Calculator/Calculator.js
@@ -1,31 +1,46 @@
-var displayDiv = document.querySelector("#display");
-displayDiv.innerText = "0";
-var firstInput='';
-var firstInput1='';
+const display = document.getElementById("display");
+let num1 = "";
+let num2 = "";
+let op = "";
 
-function press(x){
-    firstInput+=x;
-    displayDiv.innerText = firstInput;
-    return(parseFloat(firstInput));
+
+function press(num) {
+  num1 += num;
+  display.innerHTML = num1;
 }
 
-function setOP(y){
-    displayDiv.innerText = '';
-    if (y=='/'){
-        // parseFloat(a= 5/press()) ;
-        displayDiv.innerText = press();
-    }
-    else if(y=='*'){
-        var a= 5*press();
-        displayDiv.innerText = a;   
-    } 
+function setOP(key) {
+  op = key;
+  num2 = num1;
+  num1 = "";
 }
 
-function calculate(){
-
+function clr() {
+  num1 = "";
+  num2 = "";
+  op = "";
+  display.innerHTML = "0";
 }
 
-function clr(){
-
+function calculate() {
+  let a = parseFloat(num2);
+  let b = parseFloat(num1);
+  let res = 0;
+  switch(op) {
+    case "+":
+      res = a + b;
+      break;
+    case "-":
+      res = a - b;
+      break;
+    case "*":
+      res = a * b;
+      break;
+    case "/":
+      res = a / b;
+      break;
+  }
+  num1 = res;
+  op = "";
+  display.innerHTML = res;
 }
-
