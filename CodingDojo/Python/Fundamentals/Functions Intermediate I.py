b32ba3247b3acfc1ec40a1e1ab1cd8eb376aaa4f
isajudeh <isajudeh@yahoo.com>
2022-09-19 06:45:16 +0300

    python

---
+++ CodingDojo/Python/Fundamentals/Functions Intermediate I.py
@@ -0,0 +1,42 @@
+# import random
+# def randInt(min, max):
+#     num = random.random()*100
+    
+#     return num
+# print(randInt(0,100))
+# import random
+# def randInt(max):
+#     num = random.random()*max
+#     return num
+# print(randInt(50))
+# import random
+# def randInt(min):
+#     x=100-min
+#     num = random.random()* x+min
+#     return num
+# print(randInt(50))
+# import random
+# def randInt(min,max):
+#     x=max-min
+#     num = random.random()* x+min
+#     return num
+# print(randInt(10,20))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# print(randInt()) 		    # should print a random integer between 0 to 100
+# print(randInt(max=50)) 	    # should print a random integer between 0 to 50
+#print(randInt(min=50)) 	    # should print a random integer between 50 to 100
+#print(randInt(min=50, max=500))    # should print a random integer between 50 and 500
+
