4734011c3c97dff1244bfc068a05d3529a978229
isajudeh <isajudeh@yahoo.com>
2022-09-23 20:33:31 +0300

    djanog

--- CodingDojo/python_stack/django/First_Django_Project/first_app/views.py
+++ CodingDojo/python_stack/django/First_Django_Project/first_app/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import HttpResponse, redirect
 from django.http import JsonResponse
 def root(request):
     return redirect("/blogs")
-def index(request):
-    return HttpResponse("placeholder to later display a list of all blogs")
+# def index(request):
+#     return HttpResponse("placeholder to later display a list of all blogs")
 def new(request):
     return HttpResponse("placeholder to display a new form to create a new blog")
 def create(request):
@@ -16,3 +16,15 @@ def destroy(request,number):
     return redirect("/blogs")
 def josn(request):
     return JsonResponse({"response": "JSON response from redirected_method", "status": True})
+
+# -------------------------------
+from django.shortcuts import render
+# def index(request):
+#     return render(request, "index.html")
+def index(request):
+    context = {
+    "name": "Noelle",
+    "favorite_color": "turquoise",
+    "pets": ["Bruce", "Fitz", "Georgie"]
+    }
+    return render(request,"index.html", context)
