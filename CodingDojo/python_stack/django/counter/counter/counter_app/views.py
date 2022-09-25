ba746ac57360695d974b12c787659878711a5cc9
isajudeh <isajudeh@yahoo.com>
2022-09-25 10:25:32 +0300

    counter

---
+++ CodingDojo/python_stack/django/counter/counter/counter_app/views.py
@@ -0,0 +1,12 @@
+
+from django.shortcuts import render,redirect
+def index(request):
+    if 'num' in request.session:
+        request.session['num']+=1
+    else:
+        request.session['num']=1
+    return render(request,"index.html")
+
+def clear(request):
+    del request.session['num']
+    return redirect("/")
---
+++ exam/exam.js
@@ -0,0 +1,10 @@
+function remove(element){
+    document.querySelector(".cookie").remove();
+}
+function hover(element) {
+    element.setAttribute('src', 'images/assets/succulents-2.jpg');
+  }
+  
+  function unhover(element) {
+    element.setAttribute('src', 'images/assets/succulents-1.jpg');
+  }
