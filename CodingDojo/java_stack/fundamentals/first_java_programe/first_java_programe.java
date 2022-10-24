4da6608155825e0a999da9a53ec95cfd16b46e54
isajudeh <isajudeh@yahoo.com>
2022-10-24 16:31:41 +0300

    java

---
+++ CodingDojo/java_stack/fundamentals/first_java_programe/first_java_programe.java
@@ -0,0 +1,8 @@
+public class first_java_programe {
+	public static void main(String[] args) {
+        System.out.println("My name is ISA");
+        System.out.println("I am 31 years old");
+        System.out.println("My hometown is Ramallah, PS");
+	}
+    
+}
