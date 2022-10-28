5d0e3c812b1e9822bf784fdeb905ff3fc469ab0f
isajudeh <isajudeh@yahoo.com>
2022-10-28 16:55:54 +0300

    update

--- CodingDojo/java_stack/fundamentals/greeting/Test.java
+++ CodingDojo/java_stack/fundamentals/greeting/Test.java
@@ -1,14 +1,14 @@
 import java.util.ArrayList;
-class Test {
-    
-		Arraylist<Integer> Arr1 = new Arraylist<Integer>();
+public class Test {
+    public static void main(String[] args) {
+		ArrayList<Integer> Arr1 = new ArrayList<Integer>();
 		Arr1.add(1);
 		Arr1.add(2);
 		Arr1.add(1);
 		Arr1.add(3);
 		Arr1.add(4);
 		Arr1.add(2);
-		System.out.println(Arr1.get(i))
+		System.out.println(Arr1.get(i));
 		for(int i=0; i<Arr1.siza() ;i++)
 		{
 			System.out.println(Arr1.get(i));
