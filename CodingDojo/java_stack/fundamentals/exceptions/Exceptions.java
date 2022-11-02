504cc6fbd1b078a8599d7f583b6486c85ee0a32a
isajudeh <isajudeh@yahoo.com>
2022-11-02 14:38:32 +0200

    exp

---
+++ CodingDojo/java_stack/fundamentals/exceptions/Exceptions.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class Exceptions {
+        public void exp(){
+            ArrayList<Object> myList = new ArrayList<Object>();
+            myList.add("13");
+            myList.add("hello world");
+            myList.add(48);
+            myList.add("Goodbye World");
+            for(int i = 0; i < myList.size(); i++) {
+            try
+            {
+                
+                Integer castedValue = (Integer) myList.get(i);
+            }
+            catch (Exception e)
+            {
+                System.out.println("index number: "+i+" Nanana....");
+            }
+            }
+        }
+    }
+
---
+++ CodingDojo/java_stack/fundamentals/exceptions/TestExceptions.java
@@ -0,0 +1,6 @@
+public class TestExceptions {
+    public static void main(String[] args){
+    Exceptions exc= new Exceptions();
+    exc.exp();
+    }
+}
