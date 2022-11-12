650f108f5bf7d591f08b17813a26d719c3d9c53d
isajudeh <isajudeh@yahoo.com>
2022-11-12 10:31:28 +0200

    mvc

---
+++ CodingDojo/java_stack/MVC/mvc/src/test/java/com/coding_dojo/mvs/MvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coding_dojo.mvs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
