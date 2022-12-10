c5c29bb5c36c9bc8e4cac09f68ee760b24d1c17f
isajudeh <isajudeh@yahoo.com>
2022-12-10 13:01:37 +0200

    react2

--- CodingDojo/mern_stack/JSX/firstproject/src/App.js
+++ CodingDojo/mern_stack/JSX/firstproject/src/App.js
@@ -7,7 +7,10 @@ function App() {
   return (
     <div className="App">
       <List/>
-      <PersonCard firstName="ISA" lastName="Judeh" age= {31} hairColor="Black" />
+      <PersonCard firstName="ZOZO" lastName="DODO" age= {31} hairColor="Black"
+                  firstName1="LOLO" lastName1="SOSO" age1= {21} hairColor1="Black"
+                  firstName2="JOJO" lastName2="KOKO" age2= {11} hairColor2="Black"
+                  firstName3="FOFO" lastName3="MOMO" age3= {41} hairColor3="Black"/>
     </div>
   );
 }
--- CodingDojo/mern_stack/JSX/firstproject/src/components/PersonCard.js
+++ CodingDojo/mern_stack/JSX/firstproject/src/components/PersonCard.js
@@ -2,12 +2,32 @@ import React, { Component } from 'react';
 
 class PersonCard extends Component {
   render() {
-    const {firstName, lastName, age, hairColor}=this.props;
+    const {firstName, lastName, age, hairColor,
+           firstName1, lastName1, age1, hairColor1,
+           firstName2, lastName2, age2, hairColor2,
+           firstName3, lastName3, age3, hairColor3} = this.props;
     return (
     <div>
+        <div>
         <h1>{firstName}, {lastName}</h1>
         <p>Age: {age}</p>
         <p>Hair Color: {hairColor}</p>
+        </div>
+        <div>
+        <h1>{firstName1}, {lastName1}</h1>
+        <p>Age: {age1}</p>
+        <p>Hair Color: {hairColor1}</p>
+        </div>
+        <div>
+        <h1>{firstName2}, {lastName2}</h1>
+        <p>Age: {age2}</p>
+        <p>Hair Color: {hairColor2}</p>
+        </div>
+        <div>
+        <h1>{firstName3}, {lastName3}</h1>
+        <p>Age: {age3}</p>
+        <p>Hair Color: {hairColor3}</p>
+        </div>
     </div>
     );
   }
