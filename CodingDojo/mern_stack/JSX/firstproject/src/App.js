7021d1640e7ba1a48620fde736641810a672698c
isajudeh <isajudeh@yahoo.com>
2022-12-10 12:50:46 +0200

    react1

--- CodingDojo/mern_stack/JSX/firstproject/src/App.js
+++ CodingDojo/mern_stack/JSX/firstproject/src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
 import './App.css'; 
 import List from './components/List';
+import PersonCard from './components/PersonCard';
+
 
 function App() {
   return (
     <div className="App">
       <List/>
+      <PersonCard firstName="ISA" lastName="Judeh" age= {31} hairColor="Black" />
     </div>
   );
 }
--- CodingDojo/mern_stack/JSX/firstproject/src/components/List.js
+++ CodingDojo/mern_stack/JSX/firstproject/src/components/List.js
@@ -4,6 +4,9 @@ class List extends Component {
   render() {
     return (
     <div>
+        <hr></hr>
+        Task First JSX
+        <hr></hr>
         <h1>Hello Dojo!</h1>
         <h2>Things I need to do:</h2>
         <ul>
@@ -12,6 +15,9 @@ class List extends Component {
             <li>Run a Marathom</li>
             <li>Feed the cats</li>
         </ul>
+        <hr></hr>
+        Task PersonCard
+        <hr></hr>
     </div>
     );
   }
